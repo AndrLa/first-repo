cdebb8001ec08f52f99644d7c98183766d1b3533
Laura <lauraandronic88888888@gmail.com>
2024-09-27 18:42:49 +0200

    version1

---
+++ MyGigClass.java
@@ -0,0 +1,35 @@
+package first_git_package;
+
+public class MyGigClass {
+    public static void main(String[] args) {
+
+        System.out.println("Version 1");
+        /*Notes: How tu puch a git project to GitHub?
+        -git init = it creates local repository.ONCE FOR EACH PROJECT.
+        If git is not installed then you will see an error at that point.
+
+        - git status = OPTIONAL. Show changes,it's like our guide, we can use this anytime to see current conditions.
+
+        - git add . = "add" stage changes in Git repository for the next command
+
+        -git commit -m"any message" = creates a new commit.Moves code from Staging Area to Local repository
+
+        -git branch -M main = rename the current branch to main. DO THIS ONCE.
+
+        -git remote add origin <repository url> = used to connect your local git to a remote repository. DO IT ONCE.
+
+        -git push -u origin main = DO IT ONCE.
+         */
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
